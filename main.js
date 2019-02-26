54b03080301305522cabd231d0d6e90f6c683789
nebiyat-t <nebiyatzelealem@gmail.com>
2019-02-26 18:25:28 -0500

    Changed up the idea to my page, decided to do a resume instead of an about me. Also made some minor changes with CSS

---
+++ main.js
@@ -0,0 +1,42 @@
+'use strict'
+let question1 = prompt('Is your name Nebiyat? Please insert your name')
+let question2 = prompt('Do you live in Takoma Park?')
+let question3 = prompt('Is Chipotle your favorite thing to eat?')
+let question4 = prompt('Is basketball your favorite sport? Insert your favorite sport')
+let question5 = prompt('Do you playon Nintendo? Insert the gaming system you use')
+
+if(question1 === 'nebiyat'){
+console.log('Yes'.toUpperCase()) 
+}
+else{
+    console.log('No'.toUpperCase()) 
+    }
+
+if(question2 === 'No i live in silver spring')
+console.log('Yes'.toUpperCase());
+
+else{
+console.log('No'.toUpperCase())
+}
+
+if(question3 === 'yes')
+console.log('Yes'.toUpperCase());
+
+else{
+    console.log('No'.toUpperCase())
+
+}
+
+if(question4 === 'basketball')
+console.log('Yes' .toUpperCase());
+
+else{
+    console.log('No' .toUpperCase())
+}
+
+if(question5 === 'xboxone')
+console.log('Yes' .toUpperCase());
+
+else{
+    console.log('No' .toUpperCase())
+}
