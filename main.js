180638f317c419b6c086a2660db4ffd56abdd73f
nebiyat-t <nebiyatzelealem@gmail.com>
2019-02-26 18:44:56 -0500

    Updated JS code

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 'use strict'
-let question1 = prompt('Is your name Nebiyat? Please insert your name')
+let question1 = prompt('Is your name Nebiyat? Please insert your name in all Caps')
 let question2 = prompt('Do you live in Takoma Park?')
 let question3 = prompt('Is Chipotle your favorite thing to eat?')
 let question4 = prompt('Is basketball your favorite sport? Insert your favorite sport')
 let question5 = prompt('Do you playon Nintendo? Insert the gaming system you use')
 
-if(question1 === 'nebiyat'){
-console.log('Yes'.toUpperCase()) 
+if(question1 === 'nebiyat'.toUpperCase()){
+console.log('Yes');
 }
 else{
-    console.log('No'.toUpperCase()) 
+    console.log('No') 
     }
 
-if(question2 === 'No i live in silver spring')
+if(question2 === 'No i live in silver spring'.toLocaleLowerCase())
 console.log('Yes'.toUpperCase());
 
 else{
